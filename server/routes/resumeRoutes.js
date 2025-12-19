73a7336bf1117f4826be1f0659f97e250421f9e9
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2025-12-19 12:03:22 +0530

    Added AI feature in Resume Builder

---
+++ server/routes/resumeRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import  protect  from "../middlewares/authMiddleware.js";
+import { createResume, UpdateResume ,deleteResume,getResumeById,getPublicResumeById} from "../controllers/resumeController.js";
+import upload from "../configs/multer.js";
+
+const resumeRouter = express.Router();
+
+
+resumeRouter.post('/create', protect,createResume);
+resumeRouter.put('/update',  upload.single('image'),protect,UpdateResume);
+
+resumeRouter.delete('/delete/:resumeId', protect,deleteResume);
+resumeRouter.get('/get/:resumeId', protect,getResumeById);
+resumeRouter.get('/public/:resumeId', getPublicResumeById);
+
+export default resumeRouter;
+
